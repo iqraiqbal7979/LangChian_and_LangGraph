b2115da4a91a4b05780c2e2be7c08a3903b59e43
Iqra <iqraiqbal7982@gmail.com>
2026-08-16 22:14:02 +0500

    Add clean Reflection_agent project with gitignore

---
+++ Langraph_course/Reflection_agent/graph.py
@@ -0,0 +1,60 @@
+from typing import TypedDict, Annotated
+from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, StateGraph
+from langgraph.graph.message import add_messages
+
+# Hamne chains.py se chains import kar li
+from chains import generate_chain, reflect_chain
+
+load_dotenv()
+
+class MessageGraph(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+REFLECT = "reflect"
+GENERATE = "generate"
+
+def generation_node(state: MessageGraph):
+    return {"messages": [generate_chain.invoke({"messages": state["messages"]})]}
+
+def reflection_node(state: MessageGraph):
+    res = reflect_chain.invoke({"messages": state["messages"]})
+    return {"messages": [HumanMessage(content=res.content)]}
+
+builder = StateGraph(state_schema=MessageGraph)
+builder.add_node(GENERATE, generation_node)
+builder.add_node(REFLECT, reflection_node)
+builder.set_entry_point(GENERATE)
+
+def should_continue(state: MessageGraph):
+    if len(state["messages"]) > 6:
+        return END
+    return REFLECT
+
+builder.add_conditional_edges(GENERATE, should_continue)
+builder.add_edge(REFLECT, GENERATE)
+
+graph = builder.compile()
+
+if __name__ == "__main__":
+    print("Graph compiled. Starting agent...")
+    inputs = {
+        "messages": [
+            HumanMessage(
+                content="""Make this tweet better:
+                            @LangChainAI
+                            — newly Tool Calling feature is seriously underrated.
+                            After a long wait, it's here- making the implementation of agents across different models with function calling - super easy.
+                            Made a video covering their newest blog post"""
+            )
+        ]
+    }
+    
+    # Graph run karte hain
+    response = graph.invoke(inputs)
+    
+    # Final response print karte hain
+    for message in response["messages"]:
+        print(f"\n--- {message.type.upper()} ---\n")
+        print(message.content)
